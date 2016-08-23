0730942f7a60197f05a2504b01c98a7a01037f44
Dryvenn <dryvenn@gmail.com>
2016-08-23 10:28:26 +0200

    Use the GPIOs in the instruction files

--- src/multicpu_ram/instructions0.asm
+++ src/multicpu_ram/instructions0.asm
@@ -27,3 +27,24 @@ ldr r6 0x6
 ldr r7 0x7
 # see what happened
 dmp
+# set gpios to 0
+chr r0 0x0
+str r0 0x10000000
+str r0 0x10000001
+str r0 0x10000002
+str r0 0x10000003
+str r0 0x10000004
+str r0 0x10000005
+str r0 0x10000006
+str r0 0x10000007
+# load them back
+ldr r0 0x10000000
+ldr r1 0x10000001
+ldr r2 0x10000002
+ldr r3 0x10000003
+ldr r4 0x10000004
+ldr r5 0x10000005
+ldr r6 0x10000006
+ldr r7 0x10000007
+# see what happened
+dmp
